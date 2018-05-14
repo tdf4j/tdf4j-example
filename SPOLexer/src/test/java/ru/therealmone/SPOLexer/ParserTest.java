5d921c5cf5f6766ae4fa7194525d2c87a5be3dcd
therealmonE <r.fatnev@gmail.com>
2018-05-14 22:51:04 +0300

    Revert

--- SPOLexer/src/test/java/ru/therealmone/SPOLexer/ParserTest.java
+++ SPOLexer/src/test/java/ru/therealmone/SPOLexer/ParserTest.java
@@ -1,113 +1,134 @@
 package ru.therealmone.SPOLexer;
 
+import com.opencsv.CSVReader;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import ru.therealmone.SPOParser.Parser;
 import ru.therealmone.TranslatorAPI.Token;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class ParserTest {
+    private static HashMap<String, Pattern> lexemes = new HashMap<>();
+    private static HashMap<String, Integer> priorities = new HashMap<>();
+    private static HashMap<Integer, String[]> langRules = new HashMap<>();
+    private static HashMap<String, Map<String, Integer>> analyzeTable = new HashMap<>();
+
+
     @Test
     public void parserTests() {
-        Lexer lexer = new Lexer(true);
+        loadLexemes();
+        loadLang();
+
+        Lexer lexer = new Lexer(lexemes, priorities);
         HashSet<String> terminals = new HashSet<>(lexer.lexemes.keySet());
 
         //while tests
         lexer.generateTokens("while() {}");
-        assertFalse(check(lexer.tokens, new Parser(terminals)));
+        assertFalse(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("while(a > b) {}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("while((a > b) & (c < d)) {}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("while((((((a > b))))) & (((((c < d)))))) {}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("while((a == b) & (c < d * (5 + 1)) | ((10 *(s + 150) <= 300))){}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("while(((((((((((c < d))))))))))) {}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("while(a < b & c > d) {}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("while(a == b & c < d * (5 + 1) | 10 * (s + 150) <= 300){}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         //if tests
         lexer.generateTokens("if() {}");
-        assertFalse(check(lexer.tokens, new Parser(terminals)));
+        assertFalse(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("if(a > b) {}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("if((a > b) & (c < d)) {}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("if((((((a > b))))) & (((((c < d)))))) {}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("if((a == b) & (c < d * (5 + 1)) | ((10 *(s + 150) <= 300))){}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("if(((((((((((c < d))))))))))) {}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("if(a < b & c > d) {}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("if(a == b & c < d * (5 + 1) | 10 * (s + 150) <= 300){}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         //for tests
         lexer.generateTokens("for(i = 0; ; i = i + 1){}");
-        assertFalse(check(lexer.tokens, new Parser(terminals)));
+        assertFalse(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("for(i = 0; i < 100; i = i + 1) {}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("for(i = (((((a + b))))) * (((((c + d))))); i > 100; i = i + 1){}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("for(i = (((1 + 1) * (1 + 2)) * (1 + 1)) * (100 - 10); i > 100; i = i + 1){}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         //do-while tests
         lexer.generateTokens("do{}while()");
-        assertFalse(check(lexer.tokens, new Parser(terminals)));
+        assertFalse(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("do{}while(a > b)");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("do{}while((a > b) & (c < d))");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("do{}while((((((a > b))))) & (((((c < d))))))");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("do{}while((a == b) & (c < d * (5 + 1)) | ((10 *(s + 150) <= 300)))");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("do{}while(((((((((((c < d)))))))))))");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("do{}while(a < b & c > d)");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("do{}while(a == b & c < d * (5 + 1) | 10 * (s + 150) <= 300)");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         //Combinations
         lexer.generateTokens("while(a > b) {if (a < d) {do{i = i;}while(a < b)} else {for(i = i; i < 100; i = i + 1){i = i;}}}");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
 
         lexer.generateTokens("a = b; c = a; a = a;");
-        assertTrue(check(lexer.tokens, new Parser(terminals)));
+        assertTrue(check(lexer.tokens, new Parser(langRules, analyzeTable, lexemes.keySet())));
     }
 
     private boolean check(ArrayList<Token> tokens, Parser parser) {
@@ -120,5 +141,66 @@ public class ParserTest {
             System.out.println("Success: " + token.getType() + " " + token.getValue());
         }
         return true;
+    }
+
+    private static void loadLexemes() {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+            Document doc = docBuilder.parse("D:/JavaProjects/SPOTranslator/SPOLexer/src/main/resources/lexemes.xml");
+
+            Node root = doc.getDocumentElement();
+            NodeList childes = root.getChildNodes();
+
+            for (int i = 0; i < childes.getLength(); i++) {
+                Node node = childes.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    String type = element.getElementsByTagName("type").item(0).getTextContent();
+                    String template = element.getElementsByTagName("template").item(0).getTextContent();
+                    String priority = element.getAttribute("priority");
+
+                    if (!lexemes.containsKey(type)) {
+                        lexemes.put(type, Pattern.compile(template));
+                        priorities.put(type, Integer.parseInt(priority));
+                    } else
+                        System.out.println("Lexeme " + type + " already exists!");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void loadLang() {
+        try {
+            CSVReader csvReader = new CSVReader(new FileReader("D:/JavaProjects/SPOTranslator/SPOLexer/src/main/resources/langRules.csv"));
+            String[] nextLine;
+            csvReader.readNext();
+
+            while((nextLine = csvReader.readNext()) != null) {
+                try {
+                    langRules.put(Integer.parseInt(nextLine[0].trim()), nextLine[2].split("\\s\\+\\s"));
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+            csvReader.close();
+
+            csvReader = new CSVReader(new FileReader("D:/JavaProjects/SPOTranslator/SPOLexer/src/main/resources/analyzeTable.csv"));
+            String[] description = csvReader.readNext();
+
+            while((nextLine = csvReader.readNext()) != null) {
+                Map<String, Integer> tmp = new HashMap<>();
+                for (int i = 1; i < nextLine.length; i++) {
+                    tmp.put(description[i], Integer.parseInt(nextLine[i]));
+                }
+                analyzeTable.put(nextLine[0], tmp);
+            }
+            csvReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- SPOParser/src/main/java/ru/therealmone/SPOParser/Parser.java
+++ SPOParser/src/main/java/ru/therealmone/SPOParser/Parser.java
@@ -1,19 +1,17 @@
 package ru.therealmone.SPOParser;
 
-import com.opencsv.CSVReader;
 import ru.therealmone.TranslatorAPI.Token;
 import ru.therealmone.TranslatorAPI.Visitable;
 import ru.therealmone.TranslatorAPI.Visitor;
 
-import java.io.FileReader;
 import java.util.*;
 
 public class Parser implements Visitor, Visitable {
 
     private Stack<String> stack = new Stack<>();
     private Stack<Integer> stackForCNReturns = new Stack<>();
-    private Map<Integer, String[]> langRules = new HashMap<>();
-    private Map<String, Map<String, Integer>> analyzeTable = new HashMap<>();
+    private Map<Integer, String[]> langRules;
+    private Map<String, Map<String, Integer>> analyzeTable;
     private HashSet<String> terminals;
     public boolean ERROR = false;
     private TreeNode root;
@@ -32,46 +30,15 @@ public class Parser implements Visitor, Visitable {
         v.visit(OPNConverter.convertToOPN(root));
     }
 
-    public Parser(HashSet<String> terminals) {
-        this.terminals = terminals;
-        terminals.add("$");
+    public Parser(HashMap<Integer, String[]> langRules, HashMap<String, Map<String, Integer>> analyzeTable, Set<String> terminals) {
+        this.langRules = langRules;
+        this.analyzeTable = analyzeTable;
+        this.terminals = new HashSet<>(terminals);
+        this.terminals.add("$");
 
         stack.push("lang");
         root = new TreeNode("lang");
         currentTreeNode = root;
-
-        try {
-            CSVReader csvReader = new CSVReader(new FileReader("D:/JavaProjects/SPOTranslator/SPOParser/src/main/resources/langRules.csv"));
-            String[] nextLine;
-            csvReader.readNext();
-
-            while((nextLine = csvReader.readNext()) != null) {
-                try {
-                    langRules.put(Integer.parseInt(nextLine[0].trim()), nextLine[2].split("\\s\\+\\s"));
-                } catch (NumberFormatException e) {
-                        e.printStackTrace();
-                }
-            }
-            csvReader.close();
-
-            csvReader = new CSVReader(new FileReader("D:/JavaProjects/SPOTranslator/SPOParser/src/main/resources/analyzeTable.csv"));
-            String[] description = csvReader.readNext();
-
-            while((nextLine = csvReader.readNext()) != null) {
-                Map<String, Integer> tmp = new HashMap<>();
-                for (int i = 1; i < nextLine.length; i++) {
-                    tmp.put(description[i], Integer.parseInt(nextLine[i]));
-                }
-                analyzeTable.put(nextLine[0], tmp);
-            }
-            csvReader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-//        System.out.println("----------------------------------PARSER----------------------------------------");
-//        showLangRules();
-//        System.out.println("--------------------------------------------------------------------------------");
     }
 
     public void showLangRules() {
