866d35910e7239f3f4c916143f18fdec8d41f680
therealmone <r.fatnev@gmail.com>
2019-06-10 10:20:03 +0300

    tdf4j update

--- interpreter/src/test/java/io/github/therealmone/jtrAPI/InterpreterTest.java
+++ interpreter/src/test/java/io/github/therealmone/jtrAPI/InterpreterTest.java
@@ -48,10 +48,10 @@ public class InterpreterTest {
                 out
         );
         assertEquals(
-                "1.0\r\n" +
-                        "2.0\r\n" +
-                        "Can't find key one.\r\n",
-                out.toString()
+                "1.0" +
+                        "2.0" +
+                        "Can't find key one.",
+                out.toString().replaceAll("[\r\n]", "")
         );
 
 
@@ -63,7 +63,7 @@ public class InterpreterTest {
                         "put(a, i);$",
                 out
         );
-        assertEquals("Key i already exists.\r\n", out.toString());
+        assertEquals("Key i already exists.", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
@@ -81,13 +81,13 @@ public class InterpreterTest {
                 out
         );
         assertEquals(
-                "0.0\r\n" +
-                        "1.0\r\n" +
-                        "2.0\r\n" +
-                        "3.0\r\n" +
-                        "4.0\r\n" +
-                        "Index out of bound: -1. Length = 5\r\n",
-                out.toString()
+                "0.0" +
+                        "1.0" +
+                        "2.0" +
+                        "3.0" +
+                        "4.0" +
+                        "Index out of bound: -1. Length = 5",
+                out.toString().replaceAll("[\r\n]", "")
         );
 
 
@@ -97,7 +97,7 @@ public class InterpreterTest {
                         "new i = get(a, 100);$",
                 out
         );
-        assertEquals("Can't find key 100.0.\r\n", out.toString());
+        assertEquals("Can't find key 100.0.", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
@@ -106,7 +106,7 @@ public class InterpreterTest {
                         "new i = get(a, i);$",
                 out
         );
-        assertEquals("Wrong type of: i\r\n", out.toString());
+        assertEquals("Wrong type of: i", out.toString().replaceAll("[\r\n]", ""));
     }
 
     @Test
@@ -119,7 +119,7 @@ public class InterpreterTest {
                         "print(get(a, i));$",
                 out
         );
-        assertEquals("100.0\r\n", out.toString());
+        assertEquals("100.0", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
@@ -129,12 +129,12 @@ public class InterpreterTest {
                         "print(get(a, 0));$",
                 out
         );
-        assertEquals("100.0\r\n", out.toString());
+        assertEquals("100.0", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
         interpreter.process("print(\"Test String\");$", out);
-        assertEquals("Test String\r\n", out.toString());
+        assertEquals("Test String", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
@@ -143,7 +143,7 @@ public class InterpreterTest {
                         "print(\"This value equals \" ++ i);$",
                 out
         );
-        assertEquals("This value equals 100.0\r\n", out.toString());
+        assertEquals("This value equals 100.0", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
@@ -154,7 +154,7 @@ public class InterpreterTest {
                         "print(\"This value equals \" ++ i ++ \": \" ++ get(a, i));$",
                 out
         );
-        assertEquals("This value equals 100.0: 100.0\r\n", out.toString());
+        assertEquals("This value equals 100.0: 100.0", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
@@ -163,49 +163,49 @@ public class InterpreterTest {
                         "print(a);$",
                 out
         );
-        assertEquals("HashSet@hash: [  ]\r\n", out.toString().replaceAll("@\\d+", "@hash"));
+        assertEquals("HashSet@hash: [  ]", out.toString().replaceAll("@\\d+", "@hash").replaceAll("[\r\n]", ""));
     }
 
     @Test
     public void mathOperationsTest() {
         out.reset();
         interpreter.process("print(100 + 100);$", out);
-        assertEquals("200.0\r\n", out.toString());
+        assertEquals("200.0", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
         interpreter.process("print(100 * 10);$", out);
-        assertEquals("1000.0\r\n", out.toString());
+        assertEquals("1000.0", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
         interpreter.process("print(100 - 101);$", out);
-        assertEquals("-1.0\r\n", out.toString());
+        assertEquals("-1.0", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
         interpreter.process("print(100 / 5 * -1);$", out);
-        assertEquals("-20.0\r\n", out.toString());
+        assertEquals("-20.0", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
         interpreter.process("print(100 div 10);$", out);
-        assertEquals("10.0\r\n", out.toString());
+        assertEquals("10.0", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
         interpreter.process("print(105 mod 20);$", out);
-        assertEquals("5.0\r\n", out.toString());
+        assertEquals("5.0", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
         interpreter.process("print(2 * 2 + 2);$", out);
-        assertEquals("6.0\r\n", out.toString());
+        assertEquals("6.0", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
         interpreter.process("print(2 * (2 + 2));$", out);
-        assertEquals("8.0\r\n", out.toString());
+        assertEquals("8.0", out.toString().replaceAll("[\r\n]", ""));
     }
 
     @Test
@@ -213,30 +213,30 @@ public class InterpreterTest {
         out.reset();
         interpreter.process("new a = hashset;$", out);
         assertEquals(
-                "Unexpected token: Token{tag=HASHSET, value=hashset, row=1, column=8}\r\n",
-                out.toString()
+                "Unexpected token: Token{tag=HASHSET, value=hashset, row=1, column=8}",
+                out.toString().replaceAll("[\r\n]", "")
         );
 
 
         out.reset();
         interpreter.process("get(a, 1);$", out);
         assertEquals(
-                "Unexpected token: Token{tag=GET, value=get, row=1, column=0}\r\n",
-                out.toString()
+                "Unexpected token: Token{tag=GET, value=get, row=1, column=0}",
+                out.toString().replaceAll("[\r\n]", "")
         );
 
 
         out.reset();
         interpreter.process("while(a & b);$", out);
         assertEquals(
-                "Unexpected token: Token{tag=LOP, value=&, row=1, column=8}\r\n",
-                out.toString()
+                "Unexpected token: Token{tag=LOP, value=&, row=1, column=8}",
+                out.toString().replaceAll("[\r\n]", "")
         );
 
 
         out.reset();
         interpreter.process("print(i);$", out);
-        assertEquals("Can't find variable i\r\n", out.toString());
+        assertEquals("Can't find variable i", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
@@ -244,14 +244,14 @@ public class InterpreterTest {
                 "new a typeof hashset;" +
                         "print(get(a, i));$"
         , out);
-        assertEquals("Can't find key i.\r\n", out.toString());
+        assertEquals("Can't find key i.", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
         interpreter.process("@$", out);
         assertEquals(
-                "Unexpected symbol: @ ( line 1, column 1 )\r\n",
-                out.toString()
+                "Unexpected symbol: @ ( line 1, column 1 )",
+                out.toString().replaceAll("[\r\n]", "")
         );
 
 
@@ -262,7 +262,7 @@ public class InterpreterTest {
                         "put(a, i);" +
                         "put(a, i);$"
         , out);
-        assertEquals("Key i already exists.\r\n",out.toString());
+        assertEquals("Key i already exists.", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
@@ -270,7 +270,7 @@ public class InterpreterTest {
                 "new a typeof hashset;" +
                         "print(get(a, i));$"
         , out);
-        assertEquals("Can't find key i.\r\n",out.toString());
+        assertEquals("Can't find key i.", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
@@ -278,7 +278,7 @@ public class InterpreterTest {
                 "new a typeof hashset;" +
                         "put(a, i);$"
         , out);
-        assertEquals("Can't find variable: i\r\n",out.toString());
+        assertEquals("Can't find variable: i", out.toString().replaceAll("[\r\n]", ""));
 
 
         out.reset();
@@ -287,7 +287,7 @@ public class InterpreterTest {
                         "new i typeof hashset;" +
                         "put(a, i);$"
         , out);
-        assertEquals("Wrong type of: i\r\n",out.toString());
+        assertEquals("Wrong type of: i", out.toString().replaceAll("[\r\n]", ""));
     }
 
     @Test
@@ -306,11 +306,11 @@ public class InterpreterTest {
                         "print(a);$"
         , out);
         assertEquals(
-                "ArrayList@hash: [ (index = 0, value = 1.0)  (index = 1, value = 2.0) ]\r\n" +
-                        "Test\r\n" +
-                        "100.0\r\n" +
-                        "101.101\r\n"
-                ,out.toString().replaceAll("@\\d+", "@hash")
+                "ArrayList@hash: [ (index = 0, value = 1.0)  (index = 1, value = 2.0) ]" +
+                        "Test" +
+                        "100.0" +
+                        "101.101"
+                ,out.toString().replaceAll("@\\d+", "@hash").replaceAll("[\r\n]", "")
         );
 
 
@@ -326,10 +326,10 @@ public class InterpreterTest {
                         "put(a, 1);$"
         , out);
         assertEquals(
-                "ArrayList@hash: [ (index = 0, value = 1.0)  (index = 1, value = 2.0) ]\r\n" +
-                        "HashSet@hash: [  ]\r\n" +
-                        "Wrong type of: a\r\n"
-                ,out.toString().replaceAll("@\\d+", "@hash")
+                "ArrayList@hash: [ (index = 0, value = 1.0)  (index = 1, value = 2.0) ]" +
+                        "HashSet@hash: [  ]" +
+                        "Wrong type of: a"
+                ,out.toString().replaceAll("@\\d+", "@hash").replaceAll("[\r\n]", "")
         );
     }
 }
